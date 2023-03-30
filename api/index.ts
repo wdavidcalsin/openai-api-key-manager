7e4b3eb87d28e78bcd2334214658397391c7eeb3
WDavid Calsin <david2c142@gmail.com>
2023-03-29 23:49:18 -0500

    feat: add get update api key status in api

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import { Client } from '@notionhq/client';
 import express, { type Request, type Response } from 'express';
 import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from 'openai';
-import { type IRows } from './types';
+import type { IRows } from './types';
 
 dotenv.config();
 
@@ -32,29 +32,85 @@ const testOpenAiApiKey = async (openaiApiKey: string): Promise<boolean> => {
   }
 };
 
+const queryDatabase = async (databaseId: string, idApiKey: string): Promise<any> => {
+  try {
+    const response = await notion.databases.query({
+      database_id: databaseId,
+      filter: {
+        property: 'id',
+        title: {
+          contains: idApiKey,
+        },
+      },
+    });
+
+    return response.results[0].id;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
 app.get('/api/notion/openai-api-key', (req: Request, res: Response) => {
-  // if (NOTION_API_KEY === '' || NOTION_DB_ID === '') {
-  //   throw new Error('Missing notion NOTION_API_KEY or NOTION_DB_ID');
-  // }
-  // const query = await notion.databases.query({
-  //   database_id: NOTION_DB_ID ?? '',
-  // });
-  // const rows = query.results.map(restItem => restItem) as IRows[];
-  // const rowStructured = rows.map(({ properties }) => ({
-  //   id: properties?.id.title[0].text.content,
-  //   value: properties?.value.rich_text[0].text.content,
-  // }));
-  // try {
-  //   for (const structured in rowStructured) {
-  //     const openaiApiKeyIsValid = await testOpenAiApiKey(structured);
-  //     if (openaiApiKeyIsValid) {
-  //       res.status(200).send(structured);
-  //     }
-  //   }
-  // } catch (err) {
-  //   console.error(err);
-  //   res.status(500).send('Something went wrong.');
-  // }
+  void (async () => {
+    if (NOTION_API_KEY === '' || NOTION_DB_ID === '') {
+      throw new Error('Missing notion NOTION_API_KEY or NOTION_DB_ID');
+    }
+
+    const query = await notion.databases.query({
+      database_id: NOTION_DB_ID ?? '',
+    });
+
+    const rows = query.results.map(restItem => restItem) as IRows[];
+
+    const rowStructured = rows.map(({ properties }) => ({
+      id: properties?.id.title[0].text.content,
+      value: properties?.value.rich_text[0].text.content,
+    }));
+
+    try {
+      for (const structured of rowStructured) {
+        const openaiApiKeyIsValid = await testOpenAiApiKey(structured.value);
+        if (openaiApiKeyIsValid) {
+          res.status(200).send(structured);
+          return;
+        }
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Something went wrong.');
+    }
+  })();
+});
+
+app.get('/api/notion/update-openai-api-key', (req: Request, res: Response) => {
+  void (async () => {
+    if (NOTION_API_KEY === '' || NOTION_DB_ID === '') {
+      throw new Error('Missing notion NOTION_API_KEY or NOTION_DB_ID');
+    }
+
+    const { id } = req.query as { id: string };
+
+    const idPage = await queryDatabase(NOTION_DB_ID ?? '', id);
+
+    await notion.pages.update({
+      page_id: idPage,
+      properties: {
+        value: {
+          type: 'rich_text',
+          rich_text: [
+            {
+              text: {
+                content: 'New value',
+              },
+            },
+          ],
+        },
+      },
+    });
+
+    res.status(200).send({ status: 'complete', query: id, idPage });
+  })();
 });
 
 export default app;
